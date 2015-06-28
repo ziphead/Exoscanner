c3e515c83fe9d9c16e31f4fbe9c36cdb8505fdd7
ziphead <ymilto@gmail.com>
2015-06-28 17:17:05 +0800

    lxml module used

--- exo.py
+++ exo.py
@@ -1,63 +1,23 @@
-#http://www.openexoplanetcatalogue.com/systems/?filters=habitable
-import xml.etree.ElementTree as ET
-oec = ET.parse("systems.xml")
+from lxml import etree
+
 class Systems():
     def __init__(self, data):
-        self.oec = ET.parse("systems.xml")
-<<<<<<< HEAD
-        #self.root = self.oec.find("..")
+        self.dbxml = etree.parse('systems.xml')
+        self.tree = self.dbxml.xpath('/systems')
         self.data = data
     def scan(self):
-        self.child = "star"
-        self.systemz = self.oec.findall(".//system*")
-        for system in self.systemz:
-            sysnm = system.find("./name").text
-            print "system name => ", sysnm
+        for system in self.dbxml.xpath('/systems/system'):
+            print "=>",system.tag , "system ninfo : ", system.text
             for star in system:
-                starnm = star.find("./name").text
-                print "star name ==> ", starnm
+                print "===>",star.tag, "star info : ", star.text
                 for planet in star:
-                    planm = planet.find("./name").text
-                    print "planet name ===> ", starnm
-               #print element.tag, element.text, "parent is ",  nm, "\n"
+                    print "=====>", planet.tag, "planet info : ", planet.text
+                    for planetelem in planet:
+                        print "========>", planetelem.tag, "planet info : ", planetelem.text
+
+                #print element.tag, element.text, "parent is ",  nm, "\n"
 
 x = Systems("yaayay").scan()
-=======
-        self.root = self.oec.getroot()
-        self.data = data
-    def scan(self):
-        self.child = "star"
-        self.systemz = self.oec.findall(".//system")
-        for system in self.systemz:
-            for element in system:
-                if element.tag == self.child :
-                    element.text = "Something here"
-                print element.tag, element.text, self.data, "\n"
-
-x = Systems("huui").scan()
->>>>>>> 6c31697ef84d8fbbc06350b46f8e159af56640a6
-print  "====>", x
-        
-#systems = oec.findall(".//system")
-#stars = oec.findall(".//star")
-#planets = oec.findall(".//planets")
-#for star in stars :
-   # print "Star info", star.find("..//name").text
-   # for lb in star :
-        
-    #    if lb.tag == "spectraltype" or lb.tag == "name":     
-   #          try :
-    #             lb.text.decode('utf8')
-     #        except UnicodeEncodeError:
-    #             lb.text = "cannot decode to utf"
-     #   print "==>", lb.tag,"====>", lb.text,  "\n"
-                
-<<<<<<< HEAD
-=======
-    #print [planet.findtext("name"), planet.findtext("radius")]    
-    #if star.findtext("name") == "HD 240210":
-        #print star.findtext("name"), "Star type: " , type ,"\n", " mv ", star.findtext("magV"), "\n"
-    #else: b = "No type found"
-    #print star.findtext("name"), "Srar type: " , star.findtext("magV"), type ,,  
+  
 raw_input('hello')
->>>>>>> 6c31697ef84d8fbbc06350b46f8e159af56640a6
+
